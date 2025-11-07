2a94fa78156e7cb053f6da80a13a68b64cbdf922
waywardshuck <waywardshuck@gmail.com>
2025-11-07 22:18:29 +0000

    gallery-wip
    gallery flex containers need editing to shrink/grow properly, and caption toggle isn't working

---
+++ script-medialog.js
@@ -0,0 +1,10 @@
+// CAPTION TOGGLES
+function openCaption() {
+
+    /* PUTS THE ELEMENT WITH ID: inner, INTO A VARIABLE */
+		const expanded= document.getElementsByClassName('inner');
+
+    /* TOGGLES THE CAPTION OPEN */
+		// expanded - adds the open class in the css;
+		expanded.classList.add("expanded")
+}
